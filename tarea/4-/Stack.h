56eb43174fdf01d471f4fd226ff72ea348cfad31
HeidyMarin <heidy.marin@ucsp.edu.pe>
2016-11-11 12:30:31 -0500

    Update Stack.h

--- tarea/4-/Stack.h
+++ tarea/4-/Stack.h
@@ -5,21 +5,58 @@
 
 using namespace std;
 
-template<typename T>
-class Stack_{
-	
+template <class T>
+class Stack
+{
 private:
-	vector<T> Pila;
-	
+    vector<T> v;
+    friend Stack<T> operator+(const Stack<T> &x, const Stack<T> &y);
 public:
-	void if_empty();
-	T top();
-	void push(T x);
-	void pop();
-	void print();
-	friend Stack_ operator+<>(const Stack_ &k1, const Stack_ &k2);
-	
+    bool Empty() const;
+    void Push(const T &v1);
+    T Pop();
+    T Top();
+    void printStack();
 };
 
-#endif
+template <class T>
+Stack<T> operator+(const Stack<T> &x, const Stack<T> &y)
+{
+    Stack<T> res = x;
+    for (int i = 0;i< x.v.size(); i++)
+        res.v.push_back(y.v[i]);
+    return res;
+}
 
+template <class T>
+bool Stack<T>::Empty() const
+{
+    return v.empty();
+}
+
+template <class T>
+void Stack<T>::Push(const T &v1)
+{
+    return v.push_back(v1);
+}
+
+template <class T>
+T Stack<T>::Pop()
+{
+    return v.pop_back();
+}
+
+template <class T>
+T Stack<T>::Top()
+{
+    return v.back();
+}
+
+template <class T>
+void Stack<T>::printStack()
+{
+    for(int i=0; i < v.size(); i++)
+        cout << v[i] << endl;
+}
+
+#endif // STACK_H
