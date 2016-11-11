bb970d0b3b3f6b488d5a77acb73b4f0d9141c41d
HeidyMarin <heidy.marin@ucsp.edu.pe>
2016-11-11 12:46:56 -0500

    Update Grafo.cpp

--- tarea/Grafo/Grafo.cpp
+++ tarea/Grafo/Grafo.cpp
@@ -1,14 +1,14 @@
 #include "Grafo.h"
 # include <stdexcept >
 
-Grafo :: Grafo ( const vector <int > & startPoints , const vector <int > & endPoints ) {
-	if( startPoints . size () != endPoints . size ()) {
-		throw invalid_argument (" Inicio / final listas de puntos difieren en longitud ");
+Grafo :: Grafo ( const vector <int > & inicioP , const vector <int > & finalP ) {
+	if( inicioP . size () != finalP . size ()) {
+		throw invalid_argument (" Inicio no tiene el mismo tamaño que final ");
 	}
-	for( unsigned i = 0; i < startPoints . size (); i++ ) {
-		int start = startPoints [i], end = endPoints [i];
-		outgoing [ start ]. push_back (end);
-		outgoing [end ];
+	for( unsigned i = 0; i < inicioP . size (); i++ ) {
+		int inicio = inicioP [i], fin = finalP [i];
+		outgoing [ inicio ]. push_back (fin);
+		outgoing [fin ];
 	}
 }
 int Grafo :: numOutgoing ( const int nodeID ) const {
@@ -17,8 +17,8 @@ int Grafo :: numOutgoing ( const int nodeID ) const {
 
 const vector <int > & Grafo :: adjacent ( const int nodeID ) const {
 	map <int , vector <int > >:: const_iterator i = outgoing . find ( nodeID )
-		;
-	if(i == outgoing . end ()) {
+																	 ;
+	if(i == outgoing . fin ()) {
 		throw invalid_argument (" Nodo invalido ID");
 	}
 	return i-> second ;
