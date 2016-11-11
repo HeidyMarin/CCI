21dae13e712173b375f7b820766ce83eb00812d0
HeidyMarin <heidy.marin@ucsp.edu.pe>
2016-11-11 08:56:53 -0500

    Update Grafo.cpp

--- tarea/Grafo/Grafo.cpp
+++ tarea/Grafo/Grafo.cpp
@@ -1,10 +1,25 @@
 #include "Grafo.h"
+# include <stdexcept >
 
-Grafo::Grafo() {
-	
+Grafo :: Grafo ( const vector <int > & startPoints , const vector <int > & endPoints ) {
+	if( startPoints . size () != endPoints . size ()) {
+		throw invalid_argument (" Inicio / final listas de puntos difieren en longitud ");
+	}
+	for( unsigned i = 0; i < startPoints . size (); i++ ) {
+		int start = startPoints [i], end = endPoints [i];
+		outgoing [ start ]. push_back (end);
+		outgoing [end ];
+	}
+}
+int Grafo :: numOutgoing ( const int nodeID ) const {
+	return adjacent ( nodeID ). size ();
 }
 
-Grafo::~Grafo() {
-	
+const vector <int > & Grafo :: adjacent ( const int nodeID ) const {
+	map <int , vector <int > >:: const_iterator i = outgoing . find ( nodeID )
+		;
+	if(i == outgoing . end ()) {
+		throw invalid_argument (" Nodo invalido ID");
+	}
+	return i-> second ;
 }
-
