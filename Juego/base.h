7b53d4c78451b20d2fe63f0ef3f4f3b684c8415c
HeidyMarin <heidy.marin@ucsp.edu.pe>
2016-11-17 07:59:36 -0500

    Add files via upload

---
+++ Juego/base.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include "persona.cpp"
+using namespace std;
+
+class Base
+{
+private:
+	Persona *BD[100];
+	int tam;
+public:
+	Base();
+	void insertar(Persona *x);
+	void insertar(string CNombre,string CApellido,string CDNI,int CEdad);
+	void modificar(int clave,Persona *x);
+	void modificar(int clave,string CNombre,string CApellido,string CDNI,int CEdad);
+	void eliminar(int clave);
+	
+	void mostrar(void);
+};
---
+++ Juego/persona.cpp
@@ -0,0 +1,31 @@
+#include "persona.h"
+
+Persona::Persona()
+{
+	Nombre="";
+	Apellido="";
+	DNI="";
+	Edad=0;
+}
+Persona::Persona(string CNombre,string CApellido,string CDNI,int CEdad)
+{
+	Nombre=CNombre;
+	Apellido=CApellido;
+	DNI=CDNI;
+	Edad=CEdad;
+}
+void Persona::setear(string CNombre,string CApellido,string CDNI,int CEdad)
+{
+	Nombre=CNombre;
+	Apellido=CApellido;
+	DNI=CDNI;
+	Edad=CEdad;
+}
+
+void Persona::Visualizar(void)
+{	
+	cout<<"\nNombre: "<<Nombre;
+	cout<<"\nApellido: "<<Apellido;
+	cout<<"\nDNI: "<<DNI;
+	cout<<"\nEdad: "<<Edad;
+}
